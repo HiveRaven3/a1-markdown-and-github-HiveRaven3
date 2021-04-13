3d50b4e3764fe0fcd03940ed92f0921a93a91b36
Samuel <samuelamccormick@gmail.com>
2021-04-12 17:35:51 -0700

    I added the commands I will use during the assignment

--- session.sh
+++ session.sh
@@ -5,29 +5,31 @@
 # Feel free to add comments using the hash signs '#'
 
 # Print your working directory
-
+pwd
 
 # Change your directory to a folder in which you do work for this class
 # Make sure to use the "~" shortcut rather than specifying the full path
+cd ~\info201\
 
-
-# Clone your private assignment repository from GitHub to your machine
-
+# Clone your private assignment repository from GitHub to your machineg
+git clone https://github.com/info201b-sp21/a1-markdown-and-github-HiveRaven3.git
 
 # Change your directory to inside of your "a1-news-USERNAME" folder
-
+ls
+cd \a1-markdown-and-github-HiveRaven3
 
 # Make a new folder called "imgs" - you'll download an image into this folder
-
+mkdir imgs
 
 # At appropriate checkpoints, you'll need to do the following:
 # Add all new files you have added (if any) to the git 
-
+git status
+git add <file>
 
 # Make a commit, including a _descriptive_ message
-
+git commit -am <a message>
 
 # Push your change up to GitHub
-
+git push
 
 ## good luck!
